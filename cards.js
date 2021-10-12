c1fcd6cae78f38d79fce9808b95ddbb45ae8da1e
serta45 <90148410+serta45@users.noreply.github.com>
2021-10-12 10:43:15 +0300

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const card = () => {
+    return ( 
+        
+                <div classname="card">
+                    <img src="..." classname="cards-img-top" alt="..."/>
+                        <div classname="card-body">
+                            <h5 classname="card-title">Cards title 5 </h5>
+                            <p classname="card-text">This is a longer cards with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                   </div>
+             
+         
+    );
+
+};
